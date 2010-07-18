1ba42f3f8688096967592a51e0ade5eba84dee7a
Assela Pathirana <assela@pathirana.net>
2010-07-18 13:25:45 +0200

    chapter 11 (interesting) ex done

--- app/helpers/microposts_helper.rb
+++ app/helpers/microposts_helper.rb
@@ -1,2 +1,19 @@
 module MicropostsHelper
+  def wrap(content)
+    wrap_text(content)
+  end
+
+  private
+  def wrap_text(txt, col = 30)
+      txt.gsub(/(.{1,#{col}})( +|$)\n?|(.{#{col}})/,
+          "\\1\\3\n")
+  end
+
+  def wrap_long_string(text, max_width = 30)
+    zero_width_space = ""
+      regex = /.{1,#{max_width}}/
+      (text.length < max_width) ? text : 
+      text.scan(regex).join(zero_width_space)
+  end
+
 end
